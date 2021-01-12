ad803c4ef99cc9813d8e7e669dc3f9d1ac92b13e
EC2 Default User <ec2-user@ip-172-31-52-110.ec2.internal>
2021-01-12 06:53:10 +0000

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     @user = User.find(params[:id])
     @game_items = @user.game_items
--- app/models/game_item.rb
+++ app/models/game_item.rb
@@ -6,6 +6,12 @@ class GameItem < ApplicationRecord
 
   attachment :image, destroy: false
 
+  validates :name, presence: true
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 0.5
+  }, presence: true
+
   def favorited_by?(user)
     favorites.where(user_id: user).exists?
   end
